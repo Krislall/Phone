177d5afd0f4ccc76ad4da10a4d0e0b9b8974d7ca
Крис <kris@MacBook-Pro-Kris.local>
2023-09-26 15:21:31 +0700

    2 correction

--- phone.py
+++ phone.py
@@ -1,8 +1,9 @@
 def check_phone_number(x: str) -> str:
-    result = ''.join([number for number in x if number.isdigit()])
+    result = ''.join([i for i in x if i.isdigit()])
     if len(result) in {10, 11}:
-        print(((result[::-1])[:10])[::-1])
-    else:
-        raise ValueError('Number entered incorrectly')
+        return result[-10:]
+    raise ValueError('Number entered incorrectly')
+
+
 
 
